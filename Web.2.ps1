75524ccf63ff8f051b2c580fcf2855e48c8a4c8d
Jacob Kelley <exaybachay.ak@gmail.com>
2018-11-14 15:37:36 -0700

    Update Web.2.ps1

--- Web.2.ps1
+++ Web.2.ps1
@@ -1,6 +1,21 @@
-#Connect to site and read response
-$webrequest = (Invoke-WebRequest -URI "http://web-track-1.runcode.ninja/product/7" -UseBasicParsing -TimeoutSec 60)
-$content = $webrequest.content
+#Create a blank variable to store website data
+$data = ''
+
+#Connect to site and read response from main pages
+$wrcart = (Invoke-WebRequest -URI "http://web-track-1.runcode.ninja/cart" -UseBasicParsing -TimeoutSec 60)
+$wrmain = (Invoke-WebRequest -URI "http://web-track-1.runcode.ninja/" -UseBasicParsing -TimeoutSec 60)
+$cartcontent = $wrcart.content
+$maincontent = $wrmain.content
+
+$data += $cartcontent
+$data += $maincontent
+
+#Loop through product webpages and add to data variable
+for ($i=1;$i -lt 8;$i++){
+	$wrloop = (Invoke-WebRequest -URI "http://web-track-1.runcode.ninja/product/$i" -UseBasicParsing -TimeoutSec 60)
+	$loopcontent = $wrloop.content
+	$data += $loopcontent
+}
 
 #Data we're looking for:
 #RCN{who_put_this_here}
